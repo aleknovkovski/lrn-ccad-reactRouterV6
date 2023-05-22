084a3cb743f60e2ee44d2939365e9b86a8f5b831
Alek Novkovski <alek@greatcode2.com>
2023-05-22 19:44:14 +0200

    Add /articles/:title route

--- working/src/app/App.js
+++ working/src/app/App.js
@@ -24,6 +24,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/articles" element={<Articles/>}/>
         <Route path="/categories" element={<Categories/>}/>
         <Route path="/profile" element={<Profile />}/>
+        <Route path='/articles/:title' element={ <Article /> }/>
     </Route>
 ));
 
