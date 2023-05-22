011c50ff9c09eb31e93c2dd77261af6d1cb8d8ed
Alek Novkovski <alek@greatcode2.com>
2023-05-22 21:14:32 +0200

    Handle authors page, fix bug on route to

--- working/src/app/App.js
+++ working/src/app/App.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/categories" element={<Categories/>}/>
         <Route path="/profile" element={<Profile />}/>
         <Route path='/articles/:title' element={ <Article /> }/>
-        <Route path='/authors/:title' element={ <Author /> }/>
+        <Route path='/authors/:name' element={ <Author /> }/>
     </Route>
 ));
 
